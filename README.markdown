94c6b130dbdfebf61d33364f4fc50da0573fee40
Christian Biggins <cbiggins@gmail.com>
2012-06-08 11:33:01 +1000

    Adding new script for checking time logged for the day, also updated README

--- README.markdown
+++ README.markdown
@@ -1,9 +1,9 @@
-Redmine Timelogger
+This repo comprises of two scripts, one for easily and quickly logging time against a Redmine issue (logtime) and one for checking how much time has been logged for the day (checkloggedtime.)
 
+# Redmine Timelogger
 This is for logging time against Redmine tickets from the command line. Its very fast and very easy to use.
 
-## Installation
-
+### Installation
 We need to get the code...
 
 `git clone git@github.com:cbiggins/Redmine-Timelogger.git`
@@ -24,26 +24,63 @@ Also, the 'activities' array is specific to our installation, you can update the
 
 Voila! You're ready to go!
 
-## Usage
+### Usage
 
  Arguments:
  
  -i Issue number
-
  -d Date
-
  -h Hours spent
-
  -a Activity Id
-
  -c Comments
-
  -v verbose - displays confirmations and error messages
 
- 
  Examples: To log time against an issue, run the command like the following;
 
  logtime.php -i {issue_number} -h .5
 
-
  NOTE: If you ommit the activity, it will default to Development
+ 
+# Check Logged Time
+
+This is a bit more tricky to set up. You need to have PHP and Pear installed on your machine, along with Growl. 
+
+### Installation 
+This is the same as above, get the repo and configure it. The additional tasks are;
+
+Install Net_Growl from Pear;
+`sudo pear install Net_Growl`
+If you are using MAMP, you may need to provide the path to Pear, such as;
+`sudo /Applications/MAMP/bin/php/php5.2.17/bin/pear`
+Also, if you are using MAMP 2+ you may need to ditch the Pear config if you get errors;
+`sudo mv /Applications/MAMP/bin/php/php5.2.17/conf/pear.conf /Applications/MAMP/bin/php/php5.2.17/conf/pear.conf.bak`
+
+### Config
+
+Now, open your Growl app, click 'Network' and tick 'Listen for incoming notifications' - Lastly, add a password and add this in the config below.
+
+Open checkloggedtime.php in an editor and update these variables;
+
+    $me = ''; // Your full name as it appears in Redmine
+    $growl_pass = ''; // Your Growl server password, see README 
+    $full_day = 8; // How many hours makes up 100% utilisation?
+    $today = strftime("%Y-%m-%d"); // Todays date in Redmines date format
+    
+    class Time_Entry extends ActiveResource {
+      var $site = ''; // URL to Redmine
+      var $user = ''; // Redmine username
+      var $password = ''; // Redmine pass
+
+Now, you can just run this script to ensure it is working;
+`php checkloggedtime.php`
+See if you get a growl notification about todays logged time.
+
+The last step is to create the cron or scheduled task. I have mine in my crontab to run at 5pm daily;
+`crontab -e`
+Then add something like this;
+`* 17 * * * php ~/bin/logtime/checkloggedtime.php`
+
+Or, you can actually use iCal to run scripts, by creating a new event, editing it and in the 'alert' options, select 'run script' - then just schedule the event for whenever you want. 
+
+Thats it!
+
---
+++ checkloggedtime.php
@@ -0,0 +1,100 @@
+#! /usr/bin/php
+<?php
+
+require_once ('phpactiveresource/ActiveResource.php');
+
+// Install using Pear; sudo pear install Net_Growl
+require_once ('Net/Growl/Autoload.php');
+
+/**
+ * REDMINE Section
+ */
+
+$me = ''; // Your full name as it appears in Redmine
+$growl_pass = ''; // Your Growl server password, see README 
+$full_day = 8; // How many hours makes up 100% utilisation?
+$today = strftime("%Y-%m-%d"); // Todays date in Redmines date format
+
+class Time_Entry extends ActiveResource {
+  var $site = ''; // URL to Redmine
+  var $user = ''; // Redmine username
+  var $password = ''; // Redmine pass
+  var $request_format = 'xml'; // REQUIRED!
+  var $element_name = 'time_entries'; 
+}
+
+$time = new Time_Entry;
+$time->extra_params = '?period_type=2&from=' . $today . '&to=' . $today . '&limit=1000'; // limit 1000 seems excessive, but we need to make sure we get all time entries.
+
+$times = $time->find('all');
+
+$time_logged = 0;
+
+// Loop over each entry. We can't limit the API call to a particular user, so we need to compare it with the $me var
+foreach ($times as $k => $time) {
+
+  $user = $time->_data['user']; 
+  $name_attr = 'name';
+
+  // Get the user that logged this time entry
+  $time_user = (string)$user->attributes()->$name_attr;
+
+  // If the user is $me, add the logged hours to our total
+  if ($time_user == $me) {
+    $time_logged += $time->_data['hours']; 
+  }
+}
+
+// Calculate a percentage using the $full_day val from above
+$logged_percent = ($time_logged / $full_day) * 100;
+
+
+/**
+ * GROWL Section!
+ */
+
+// Notification Type definitions
+define('GROWL_NOTIFY_STATUS',   'STATUS');
+define('GROWL_NOTIFY_ERROR',    'ERROR');
+
+// define a PHP application that sends notifications to Growl
+$appName = 'PEAR/Net_Growl ' . basename(__FILE__, '.php');
+
+$notifications = array(
+    GROWL_NOTIFY_STATUS => array(
+        'display' => 'Status',
+    ),
+    GROWL_NOTIFY_ERROR => array(
+        'display' => 'Error',
+    ),
+);
+
+$password = $growl_pass;
+$options  = array(
+    'protocol' => 'gntp', 'timeout' => 15,
+    'AppIcon'  => 'http://www.redmine.org/attachments/3462/redmine_fluid_icon.png',
+);
+
+try {
+  // Create our Growl notification and display it.
+
+  $growl = Net_Growl::singleton($appName, $notifications, $password, $options);
+  $growl->register();
+
+  $title       = 'Todays time';
+  if ($time_logged > 0) {
+    $name        = GROWL_NOTIFY_STATUS;
+    $description = $me . ', You have only logged ' . $time_logged . 'hrs (' . $logged_percent . '%) today!';
+  }
+  else {
+    $name        = GROWL_NOTIFY_ERROR;    
+    $description = $me . ', You have not logged any time today!'; 
+  }
+
+  $options     = array('sticky' => TRUE);
+
+  $growl->publish($name, $title, $description, $options);
+
+} catch (Net_Growl_Exception $e) {
+  echo 'Caught Growl exception: ' . $e->getMessage() . PHP_EOL;
+}
